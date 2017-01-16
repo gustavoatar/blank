e8080c9dfa484f36b1eaefaf929f58ad84bb577b
Gustavo <gustavo.atar@gmail.com>
2017-01-16 09:59:08 -0500

    Spiked Seltzer
    part of a pitch - ux concepts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(window).scroll(function(){
+	var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
+	var scrolltrigger = 0.95;
+	var percentageScrolled= (wintop/(docheight-winheight))*(100);
 
+	console.log('wintop='+wintop);
+	console.log('docheight='+docheight);
+	console.log('winheight='+winheight);
+	console.log(wintop+'=='+(docheight-winheight));
+	console.log(wintop==(docheight-winheight));
+	console.log(percentageScrolled);
+	if(percentageScrolled < 20){
+		$(".first-image").removeClass('fixone');
+		$(".second-image").removeClass('fixtwo');		
+	} 
+	else if(percentageScrolled >= 21){
+		$(".first-image").addClass('fixone');
+		$(".second-image").removeClass('fixtwo');		
+	} 
+	if(percentageScrolled >= 80){
+		$(".second-image").addClass('fixtwo');
+		$(".first-image").removeClass('fixone');	
+	}
+
+}); 
